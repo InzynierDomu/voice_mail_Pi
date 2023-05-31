f20f0e1d683b59ffd200bb15b5b5fa8a65d61f51
Inżynier Domu <inzynier.domu@gmail.com>
2023-05-31 22:22:17 +0200

    working version

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import subprocess
-import datetime
+import time
+import pygame
+from datetime import datetime
 import RPi.GPIO as GPIO
 
-button_pin = 17
-led_pin = 27
+button_pin = 1
+led_pin = 12
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.output(led_pin, GPIO.HIGH)
+audio_status = subprocess.run(
+    ['arecord', '-l'], capture_output=True, text=True)
+if audio_status.returncode == 0:
+    with open('/home/pi/output.txt', 'w') as file:
+        file.write(audio_status.stdout)
+else:
+    with open('/home/pi/output.txt', 'w') as file:
+        file.write("error with audio hw")
 
 while True:
-    # zmienic na sprawdzenie zbocza narastajacego
-    if GPIO.input(button_pin):
-        subprocess.Popen(["aplay", "voice_mail_greetings.wav"])
+    GPIO.output(led_pin, GPIO.LOW)
+    GPIO.wait_for_edge(button_pin, GPIO.RISING)
+    time.sleep(2)
+    if GPIO.input(button_pin) == GPIO.HIGH:
+        pygame.init()
+        pygame.mixer.music.load("/home/pi/welcome_record.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+        pygame.mixer.music.load("/home/pi/signal.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+        pygame.quit()
         GPIO.output(led_pin, GPIO.HIGH)
-        now = datetime.datetime.now()
-        date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
-        file_name = f"nagranie_{date_time}.wav"
-        process = subprocess.Popen(["arecord", "-D", "plughw:1,0", "--duration=60", "--format=cd", file_name])
-        GPIO.output(led_pin, GPIO.LOW)
-
-    #sprawdzenie czy zostało przerwane
-    #GPIO.output(led_pin, GPIO.LOW)
-    #process.terminate()
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = "/home/pi/record_" + timestamp + ".wav"
+        process = subprocess.Popen(
+            ["arecord", "-D", "plughw:1,0", "--duration=360", "--format=cd", filename])
+        GPIO.wait_for_edge(button_pin, GPIO.FALLING)
+        process.terminate()
