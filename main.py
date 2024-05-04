b155625fc5e3a017981b15f90c23a658dc0aa62a
Inżynier Domu <inzynier.domu@gmail.com>
2024-05-04 22:12:57 +0200

    add config.txt and load

--- main.py
+++ main.py
@@ -10,15 +10,30 @@ led_pin = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)
+def read_config_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                if ':' in line:
+                    key, value = line.strip().split(':', 1)
+                    globals()[key.strip()] = value.strip()
+    except Exception as e:
+        print("Error:", e)
+
+# Wywołanie funkcji z nazwą pliku konfiguracyjnego
+read_config_file("config.txt")
+
+def write_log(log)
+    with open('log.txt', 'w') as file:
+        file.write(log)
 
 audio_status = subprocess.run(
     ['arecord', '-l'], capture_output=True, text=True)
 if audio_status.returncode == 0:
-    with open('/home/pi/output.txt', 'w') as file:
-        file.write(audio_status.stdout)
+    write_log(audio_status.stdout)
 else:
-    with open('/home/pi/output.txt', 'w') as file:
-        file.write("error with audio hw")
+    write_log("error with audio card")
 
 while True:
     GPIO.output(led_pin, GPIO.LOW)
@@ -26,18 +41,24 @@ while True:
     time.sleep(2)
     if GPIO.input(button_pin) == GPIO.HIGH:
         pygame.init()
-        pygame.mixer.music.load("/home/pi/welcome_record.wav")
+        try:
+            pygame.mixer.music.load(nazwa_pliku_z_powitaniem)
+        except:
+            write_log("błędna ściezka do pliku z powitaniem")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             continue
-        pygame.mixer.music.load("/home/pi/signal.wav")
+        try:
+            pygame.mixer.music.load(nazwa_pliku_z_sygnalem)
+        except:
+            write_log("błędna ściezka do pliku z sygnałem")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
             continue
         pygame.quit()
         GPIO.output(led_pin, GPIO.HIGH)
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        filename = "/home/pi/record_" + timestamp + ".wav"
+        filename = timestamp + ".wav"
         process = subprocess.Popen(
             ["arecord", "-D", "plughw:1,0", "--duration=360", "--format=cd", filename])
         GPIO.wait_for_edge(button_pin, GPIO.FALLING)
